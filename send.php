0b1e5bf02d307fef91133960c6fa271a8fc3c0f6
ilia <gruzdev_ilya16@mail.ru>
2022-12-18 22:58:38 +0300

    commit

--- send.php
+++ send.php
@@ -1,17 +1,38 @@
 <?php
-$fio = $_POST['fio'];
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+$name = $_POST['fio'];
 $email = $_POST['email'];
-$fio = htmlspecialchars($fio);
-$email = htmlspecialchars($email);
-$fio = urldecode($fio);
-$email = urldecode($email);
-$fio = trim($fio);
-$email = trim($email);
-//echo $fio;
-//echo "<br>";
-//echo $email;
-if (mail("gruzdev_ilya16@mail.ru", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: info@webriz.ru \r\n"))
- {     echo "сообщение успешно отправлено";
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'dzharuzov@bk.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = '$wer124%'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+
+$mail->setFrom('dzharuzov@bk.ru'); // от кого будет уходить письмо?
+$mail->addAddress('gruzdev_ilya16@mail.ru');     // Кому будет уходить письмо 
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Заявка с тестового сайта';
+$mail->Body    = '' .$name . ' оставил заявку, <br>Почта этого пользователя: ' .$email;
+$mail->AltBody = '';
+
+if(!$mail->send()) {
+    echo 'Возникли ошибки';
 } else {
-    echo "при отправке сообщения возникли ошибки";
-}?>
+    echo('location: index.html');
+}
+?>
