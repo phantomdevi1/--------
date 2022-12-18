5b0eded3fd00eb113c00cbad9345783acba35a03
ilia <gruzdev_ilya16@mail.ru>
2022-12-18 19:55:50 +0300

    first commit

---
+++ form.php
@@ -0,0 +1,17 @@
+<?php
+$fio = $_POST['fio'];
+$email = $_POST['email'];
+$fio = htmlspecialchars($fio);
+$email = htmlspecialchars($email);
+$fio = urldecode($fio);
+$email = urldecode($email);
+$fio = trim($fio);
+$email = trim($email);
+mail("gruzdev_ilya16@mail.ru", "Запрос на рассылку", "Имя:".$fio.". E-mail: ".$email ,"From: grenochki@gmail.com \r\n");
+
+if(mail("gruzdev_ilya16@mail.ru", "Запрос на рассылку", "Имя:".$fio.". E-mail: ".$email ,"From: grenochki@gmail.com \r\n"))\
+  {
+    echo "сообщение успешно отправлено";
+} else {
+    echo "при отправке сообщения возникли ошибки";
+  }?>
